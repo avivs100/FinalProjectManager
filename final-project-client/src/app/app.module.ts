5fb415bb33481ad6379e149d73a558099351dcfa
אביב שיכמן <avivshichman@gmail.com>
2022-12-01 17:42:13 +0200

    create blank pages

--- final-project-client/src/app/app.module.ts
+++ final-project-client/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RippleModule } from 'primeng/ripple';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
+import { HomeComponent } from './layout/home/home/home.component';
+import { SidebarComponent } from './layout/sidebar/sidebar/sidebar.component';
+import { ToolbarComponent } from './layout/toolbar/toolbar/toolbar.component';
+import { LoginComponent } from './pages/login/login/login.component';
+import { RegisterComponent } from './pages/register/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule,
+    BrowserAnimationsModule,
+    CalendarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
