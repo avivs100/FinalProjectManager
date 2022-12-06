2557c9efdd3171d440b664b612bb477486f40ccf
אביב שיכמן <avivshichman@gmail.com>
2022-12-06 18:30:25 +0200

    fix

--- final-project-client/src/app/services/login-service.service.ts
+++ final-project-client/src/app/services/login-service.service.ts
@@ -5,17 +5,17 @@ import { DataProvaiderService } from './data-provaider.service';
 @Injectable({
   providedIn: 'root',
 })
-export class LoginServiceService {
+export class LoginService {
   constructor(private data: DataProvaiderService) {}
 
-  public connerctedUser: User | null = null;
+  public connectedUser: User | null = null;
 
   public login(id: number, pass: string): boolean {
     var user = this.data.checkLogin(id, pass);
     if (user == null) {
       return false;
     }
-    this.connerctedUser = user;
+    this.connectedUser = user;
     return true;
   }
 }
