f85c6d706700d60f2383a21e05bf7785859228e1
sagifishman1@gmail.com <sagifishman1@gmail.com>
2022-12-13 15:45:32 +0200

    add admin controller

---
+++ FinalProjectManger-serverApp/FinalProjectManger-server/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Data;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FinalProjectManger_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        static UsersDbContext context = new UsersDbContext();
+        List<Admin> admins = context.Set<Admin>().ToList();
+        // GET: api/<AdminController>
+        [HttpGet]
+        public IEnumerable<Admin> Get()
+        {
+            return admins;
+        }
+
+        // GET api/<AdminController>/5
+        [HttpGet("{id}")]
+        public Admin Get(long id)
+        {
+            var admin = admins.Find(x => x.id == id);
+            return admin;
+        }
+    }
+}
