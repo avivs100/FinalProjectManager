00cf75fb948aaa254d895dcb64b91e1c27df4623
sagifishman1@gmail.com <sagifishman1@gmail.com>
2022-12-13 23:41:13 +0200

    add schedule date conroller

---
+++ FinalProjectManger-serverApp/Domain/ScheduleDates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ScheduleDates
+    {
+        public ScheduleDates() { }
+        public ScheduleDates(DateTime date1, DateTime date2)
+        {
+            this.id = new Random().Next();
+            this.date1 = date1;
+            this.date2 = date2;
+        }
+
+        public int id { get; set; }
+        public DateTime date1 { get; set; }
+        public DateTime date2 { get; set; }
+    }
+
+    public class ScheduleDatesDetails
+    {
+        public ScheduleDatesDetails() { }
+        public ScheduleDatesDetails(DateTime date1, DateTime date2)
+        {
+            this.date1 = date1;
+            this.date2 = date2;
+        }
+
+        public DateTime date1 { get; set; }
+        public DateTime date2 { get; set; }
+    }
+}
