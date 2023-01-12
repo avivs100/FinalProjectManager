6fd0a124ac85d791a1aeb2fa3607031ed6f1e744
אביב שיכמן <avivshichman@gmail.com>
2023-01-12 21:04:07 +0200

    da

--- final-project-client/src/app/pages/schedule-page/schedule-page.component.ts
+++ final-project-client/src/app/pages/schedule-page/schedule-page.component.ts
@@ -13,10 +13,8 @@ import { StateService } from 'src/app/services/state.service';
 export class ScheduleComponent implements OnInit {
   public userType: UserType | undefined;
   constructor(public state: StateService, private api: GeneralApiService) {
-    this.userType = state.connectedUser?.userType;
+    this.userType = state.connectedUser!.userType;
   }
-  ngOnInit(): void {
-    this.schedule = this.state.schedule;
-  }
-  public schedule: ScheduleFull | null = null;
+
+  ngOnInit(): void {}
 }
