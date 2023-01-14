c3f66b12b70b170292513a56bbc487bbd6b8d72a
אביב שיכמן <avivshichman@gmail.com>
2023-01-14 11:41:38 +0200

    da

--- final-project-client/src/app/pages/schedule-page/schedule-details/session-details/session-details.component.ts
+++ final-project-client/src/app/pages/schedule-page/schedule-details/session-details/session-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { Session } from 'src/app/models/schedule-models';
+import { StateService } from 'src/app/services/state.service';
 
 @Component({
   selector: 'app-session-details',
@@ -7,8 +8,38 @@ import { Session } from 'src/app/models/schedule-models';
   styleUrls: ['./session-details.component.scss'],
 })
 export class SessionDetailsComponent implements OnChanges {
+  constructor(private state: StateService) {}
   ngOnChanges(): void {
     console.log('eee', this.session);
+    switch (this.number) {
+      case 1:
+        this.time = '08:00 - 10:00';
+        break;
+      case 2:
+        this.time = '10:00 - 12:00';
+        break;
+      case 3:
+        this.time = '12:00 - 14:00';
+        break;
+      case 4:
+        this.time = '14:00 - 16:00';
+        break;
+      case 5:
+        this.time = '16:00 - 18:00';
+        break;
+      default:
+        break;
+    }
+    if (
+      this.session!.lecturer2.id == this.state.connectedUser!.id ||
+      this.session!.lecturer3.id == this.state.connectedUser!.id ||
+      this.session!.responsibleLecturer.id == this.state.connectedUser!.id
+    ) {
+      this.itsMySession = true;
+    }
   }
   @Input() session: Session | null = null;
+  @Input() number: Number | null = null;
+  public time: string = '';
+  public itsMySession: boolean = false;
 }
